a0cfe9216aca5a5b39cc701627863c26e2c4eea8
alateeqi <jadm20066@gmail.com>
2023-09-12 10:05:15 +0300

    alateeqi
    final project

--- README.md
+++ README.md
@@ -5,14 +5,14 @@
 <div dir="rtl">
   
 ### اسم الطالب
-
+جنى ضاري ليث العتيقي
 
 ### اسم المشروع
-
+المخ الكمشوي 
 
 # شرح الفكرة
 المشكلة والحلول المطروحة إن وجدت
-
+مثلا يعني اسال ما هي عاصمة الكويت واعطي اختيارات واذا جاوب صح اروح حق السؤال اللي بعده و اذا جاوب غلاط  اقولة ان الجواب غلاط
 
 # الأعمال المستقبلية
 
@@ -21,14 +21,13 @@
 
 > Fill out project information in English ⬇️
 ### Student Name
-
+jana dhari alateqi
 
 ### Project Name
-
+frosted mind
 # Idea
 Problem and solutions if found 
 
-
+For example, I  ask what is the capital of Kuwait and give choices, and if he answers correctly, I go right to the next question, and if he answers wrong, I say that the answer is wrong.
 # Future Work 
-
-
+print()
---
+++ final project.py
@@ -0,0 +1,101 @@
+print ("Which of the Great Lakes is the deepest?")
+print("lake huron\nlake michign\nlake ontario\nlake superior ")
+trueAnswer = "lake superior"
+      
+userAnswer = input(print("what is your answer?"))
+if trueAnswer == userAnswer:
+    print("correct")
+else:
+    print("wrong")
+
+print("where is the broadway?")
+print("bostan\nlos angeles\nnew york")
+trueAnswer = " new york"
+userAnswer = input(print("what is you arnswer?"))
+if trueAnswer == userAnswer:
+    print("correct")
+else:
+    print("wrong")
+
+    print ("where and when did the olympic games start?")
+print("paris in 1900\namsterdam in 1928\nathen in 1896")
+
+trueAnswer = "athen in 1896"
+      
+userAnswer = input(print("what is your answer?"))
+if trueAnswer == userAnswer:
+    print("correct")
+else:
+    print("wrong")
+
+print ("whi laughs last,...?")
+print("laughs never\nlaughe always\nlaughs best")
+
+trueAnswer = "laughe best"
+      
+userAnswer = input(print("what is your answer?"))
+if trueAnswer == userAnswer:
+    print("correct")
+else:
+    print("wrong")
+
+print ("what's the name of a famous desert in USA?")
+print("death vally\ngreen vally\nsilicon vally")
+
+trueAnswer = "death valley"
+      
+userAnswer = input(print("what is your answer?"))
+if trueAnswer == userAnswer:
+    print("correct")
+else:
+    print("wrong")
+
+print ("how old was Qween Mum?")
+print("101years\n95years\n100years")
+
+trueAnswer = "101years"
+      
+userAnswer = input(print("what is your answer?"))
+if trueAnswer == userAnswer:
+    print("correct")
+else:
+    print("wrong")
+
+print ("who was the first man o the moon?")
+print("neil arstrong\ngerman titow\njuri gagarin")
+
+trueAnswer = "neil arsmstrong"
+userAnswer = input(print("what is your answer?"))
+if trueAnswer == userAnswer:
+    print("correct")
+else:
+    print("wrong")
+
+print ("what is the biggest shark earth?")
+print("tiger shark\nwhale skark\nwhite shark")
+
+trueAnswer = "whale shark"
+      
+userAnswer = input(print("what is your answer?"))
+if trueAnswer == userAnswer:
+    print("correct")
+else:
+    print("wrong")
+
+print ("in which film did leonard diْd not act?")
+print("titanic\nroeo and juliet\narmegddon")
+trueAnswer = "armegeddon"
+userAnswer = input(print("what is your answer?"))
+if trueAnswer == userAnswer:
+    print("correct")
+else:
+    print("wrong")
+
+print ("in which flag can you see the union jack?")
+print("USA\ncanada\naustralia")
+trueAnswer = "armegeddon"
+userAnswer = input(print("what is your answer?"))
+if trueAnswer == userAnswer:
+    print("correct")
+else:
+    print("wrong")
